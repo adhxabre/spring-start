51e7a08c5d9baca791265237533021a3a16be897
adhxabre <abeldustin06@gmail.com>
2022-10-18 13:28:19 +0700

    Making Hello World

---
+++ main.java
@@ -0,0 +1,8 @@
+// main class
+public class main {
+    // void whose run the logic code inside
+    public static void main(String[] args) {
+        // make a log on console using System.out.println which means printline
+        System.out.println("Hello world!");
+    }
+}
